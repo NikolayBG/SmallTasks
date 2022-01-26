4736f07cc42d4e0e364bf32b85987df322c5a4ad
Nicky Kostov <nicky@Nickys-MacBook-Air.local>
2022-01-26 23:04:55 +0200

    First Task

---
+++ Chapter1EvenOdd/Chapter1EvenOdd/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Chapter1EvenOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 24;
+            bool even = num % 2 == 0 ? true : false;
+            Console.WriteLine("{0} is even ? {1}", num, even);
+        }
+    }
+}
